e8277f8dc5fb5fdbe4ca58eb26a39db425765be9
Wenqing Wang <wenqing.wang@ufz.de>
2015-02-02 16:29:16 +0100

    [MeL] Add getMaximumNConnectedNodesToNode().

--- MeshLib/NodePartitionedMesh.h
+++ MeshLib/NodePartitionedMesh.h
@@ -18,14 +18,12 @@
 
 #include <vector>
 #include <string>
+#include <algorithm>
 
-#include "Mesh.h"
+#include "Node.h"
 
 namespace MeshLib
 {
-class Node;
-class Element;
-
 /// A subdomain mesh.
 class NodePartitionedMesh : public Mesh
 {
@@ -118,6 +116,20 @@ class NodePartitionedMesh : public Mesh
         std::size_t getNNonGhostElements() const
         {
             return _n_nghost_elem;
+        }
+
+        /// Get the maximum number of connected nodes to node.
+        std::size_t getMaximumNConnectedNodesToNode() const
+        {
+            std::vector<Node*>::const_iterator it_max_ncn
+                 = std::max_element(this->_nodes.cbegin(), this->_nodes.cend(),
+                                [](Node const*const node_a, Node const*const node_b)
+                                  {
+                                      return (   node_a->getConnectedNodes().size()
+                                               < node_b->getConnectedNodes().size() );
+                                  }
+                                );
+            return (*it_max_ncn)->getConnectedNodes().size() + 1;
         }
 
     private:
