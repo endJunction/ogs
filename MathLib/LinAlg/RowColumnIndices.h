712682f4c7695a552eb5f99a9d0bf77b7cf805cb
Dmitri Naumov <dmitri.naumov@ufz.de>
2015-02-13 12:08:02 +0100

    [MaL] Handle ghost ids in RowColumnIndices.
    Use generalized index type.

--- MathLib/LinAlg/RowColumnIndices.h
+++ MathLib/LinAlg/RowColumnIndices.h
@@ -16,15 +16,26 @@ namespace MathLib
 {
 
 template <typename IDX_TYPE>
-struct RowColumnIndices
+class RowColumnIndices
 {
+public:
 	typedef typename std::vector<IDX_TYPE> LineIndex;
-	RowColumnIndices(LineIndex const& rows_, LineIndex const& columns_)
-		: rows(rows_), columns(columns_)
+	RowColumnIndices(LineIndex const& rows_, LineIndex const& columns_,
+		const std::vector<IDX_TYPE>* non_ghost_local_ids_ = nullptr)
+		: rows(rows_), columns(columns_), non_ghost_local_ids(non_ghost_local_ids_)
 	{ }
 
+	std::vector<IDX_TYPE> const* getNonGhostLocalIds() const
+	{
+		return non_ghost_local_ids;
+	}
+
+public:
 	LineIndex const& rows;
 	LineIndex const& columns;
+
+private:
+	std::vector<IDX_TYPE> const* non_ghost_local_ids;
 };
 
 } // MathLib
