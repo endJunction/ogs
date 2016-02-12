726bc740d78df464dc03d5d638b63a0f1296afd9
Dmitri Naumov <dmitri.naumov@ufz.de>
2016-02-12 18:17:01 +0100

    [PL] Move getting of mesh property in process var.

--- ProcessLib/Process.h
+++ ProcessLib/Process.h
@@ -269,31 +269,11 @@ private:
 		_x->copyValues(x_copy);
 
 		std::size_t const n = _mesh.getNNodes();
-		for (ProcessVariable const& pv : _process_variables)
+		for (ProcessVariable& pv : _process_variables)
 		{
-			std::string const property_name = pv.getName();
-			// Get or create a property vector for results.
-			boost::optional<MeshLib::PropertyVector<double>&> result;
-			if (_mesh.getProperties().hasPropertyVector(property_name))
-			{
-				result =
-				    _mesh.getProperties().template getPropertyVector<double>(
-				        property_name);
-			}
-			else
-			{
-				result = _mesh.getProperties()
-				             .template createNewPropertyVector<double>(
-				                 property_name, MeshLib::MeshItemType::Node);
-			}
-
-			assert(result);
+			auto& output_data = pv.getOrCreateMeshProperty();
 
 			int const n_components = pv.getTupleSize();
-			// result's resize function is from std::vector not accounting the
-			// tuple size.
-			result->resize(x_copy.size() * n_components);
-
 			for (std::size_t i = 0; i < n; ++i)
 			{
 				MeshLib::Location const l(_mesh.getID(),
@@ -304,7 +284,7 @@ private:
 					auto const global_index =
 					    std::abs(_local_to_global_index_map->getGlobalIndex(
 					        l, component_id));
-					(*result)[i] = (x_copy)[global_index];
+					output_data[i] = (x_copy)[global_index];
 				}
 			}
 		}
