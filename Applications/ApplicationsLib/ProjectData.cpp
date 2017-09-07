0969f837faa896ea1d9aafbb01f8dec84adaf0f0
Norbert Grunwald <n.g.boettcher@gmail.com>
2017-09-07 12:54:19 +0200

    construction of Medium class now depends on config tree

--- Applications/ApplicationsLib/ProjectData.cpp
+++ Applications/ApplicationsLib/ProjectData.cpp
@@ -320,7 +320,7 @@ void ProjectData::parseMaterials(BaseLib::ConfigTree const& media_config)
     }
 
     auto const medium_config = media_config.getConfigSubtree("medium");
-    MaterialPropertyLib::Medium medium;
+    MaterialPropertyLib::Medium medium(medium_config);
 
 }
 
