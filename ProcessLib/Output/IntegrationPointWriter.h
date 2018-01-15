71f57636ae78400b12d398f167049323d8aebc71
Dmitri Naumov <github@naumov.de>
2018-01-15 20:57:41 +0100

    [PL] Write integration point data.

--- ProcessLib/Output/IntegrationPointWriter.h
+++ ProcessLib/Output/IntegrationPointWriter.h
@@ -1,4 +1,6 @@
 /**
+ * \file
+ *
  * \copyright
  * Copyright (c) 2012-2018, OpenGeoSys Community (http://www.opengeosys.org)
  *            Distributed under a Modified BSD License.
@@ -6,8 +8,15 @@
  *              http://www.opengeosys.org/project/license
  *
  */
+#include <memory>
+#include <vector>
 
 #pragma once
+
+namespace MeshLib
+{
+class Mesh;
+}
 
 namespace ProcessLib
 {
@@ -19,6 +28,22 @@ struct IntegrationPointWriter
     virtual int integrationOrder() const = 0;
     virtual std::string name() const = 0;
     virtual std::vector<std::vector<double>> values() const = 0;
+};
+
+/// Add integration point data the the mesh's properties.
+///
+/// Integration point data stored as field data (contrary to point or cell
+/// data). The data is supplemented with information in JSON format.
+void addIntegrationPointWriter(
+    MeshLib::Mesh& mesh,
+    std::vector<std::unique_ptr<IntegrationPointWriter>> const&
+        integration_point_writer);
+
+struct IntegrationPointMetaData
+{
+    std::string const name;
+    int const n_components;
+    int const integration_order;
 };
 
 }  // namespace ProcessLib
