a633c68ffd4e740bf0895f44ea3e0a750f12f34a
KeitaYoshioka <keita.yoshioka@ufz.de>
2018-01-22 10:01:50 +0100

    [T] PF: Add test cases.

--- ProcessLib/PhaseField/Tests.cmake
+++ ProcessLib/PhaseField/Tests.cmake
@@ -10,3 +10,29 @@ AddTest(
     expected_cube_1e0_pcs_0_ts_10000_t_1.000000.vtu cube_1e0_pcs_0_ts_10000_t_1.000000.vtu displacement displacement 1e-6 1e-6
     expected_cube_1e0_pcs_0_ts_10000_t_1.000000.vtu cube_1e0_pcs_0_ts_10000_t_1.000000.vtu phasefield phasefield 1e-6 1e-6
    )
+
+AddTest(
+    NAME PhaseField_2D_StaticHydraulicFracture
+    PATH PhaseField
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS 2D_static.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
+    DIFF_DATA
+    expected_2D_StaticCrack_pcs_1_ts_1_t_1.000000.vtu 2D_StaticCrack_pcs_1_ts_1_t_1.000000.vtu displacement displacement 1e-15 1e-15
+    expected_2D_StaticCrack_pcs_1_ts_1_t_1.000000.vtu 2D_StaticCrack_pcs_1_ts_1_t_1.000000.vtu phasefield phasefield 1e-15 1e-15
+   )
+
+AddTest(
+    NAME PhaseField_3D_beam_LARGE
+    PATH PhaseField
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS beam3d_stag_1pcs.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
+    DIFF_DATA
+    expected_beam3_stag1pcsAT2_pcs_1_ts_10_t_1.000000.vtu beam3_stag1pcsAT2_pcs_1_ts_10_t_1.000000.vtu displacement displacement 1e-15 1e-15
+    expected_beam3_stag1pcsAT2_pcs_1_ts_10_t_1.000000.vtu beam3_stag1pcsAT2_pcs_1_ts_10_t_1.000000.vtu phasefield phasefield 1e-15 1e-15
+   )
