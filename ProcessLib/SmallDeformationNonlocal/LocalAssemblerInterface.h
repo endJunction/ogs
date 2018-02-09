0bb2eff8a296ede40115e04afd9cb9ac78cf571b
Dmitri Naumov <dmitri.naumov@ufz.de>
2018-02-09 12:12:56 +0100

    [PL] SDN: Add ip damage initialization from cell data.

--- ProcessLib/SmallDeformationNonlocal/LocalAssemblerInterface.h
+++ ProcessLib/SmallDeformationNonlocal/LocalAssemblerInterface.h
@@ -28,6 +28,8 @@ struct SmallDeformationNonlocalLocalAssemblerInterface
 {
     virtual void setIPDataInitialConditions(std::string const& name,
                                             double const* values) = 0;
+    virtual void setIPDataInitialConditionsFromCellData(
+        std::string const& name, std::vector<double> const& value) = 0;
 
     virtual void computeCrackIntegral(
         std::size_t mesh_item_id,
