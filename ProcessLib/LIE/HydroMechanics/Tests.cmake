837fdfdd14d13b277b5b6276bc510ba01052264e
Dmitri Naumov <dmitri.naumov@ufz.de>
2018-03-21 19:47:21 +0100

    [T] LIE/HM: Add forgotten GLOB.

--- ProcessLib/LIE/HydroMechanics/Tests.cmake
+++ ProcessLib/LIE/HydroMechanics/Tests.cmake
@@ -61,21 +61,21 @@ AddTest(
     TESTER vtkdiff
     REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
     DIFF_DATA
-    single_fracture_constK_pcs_0_ts_*.vtu pressure pressure 1e-16 1e-16
-    single_fracture_constK_pcs_0_ts_*.vtu pressure_interpolated pressure_interpolated 1e-16 1e-16
-    single_fracture_constK_pcs_0_ts_*.vtu displacement displacement 1e-16 0
-    single_fracture_constK_pcs_0_ts_*.vtu displacement_jump1 displacement_jump1 1e-16 0
-    single_fracture_constK_pcs_0_ts_*.vtu nodal_w nodal_w 1e-16 0
-    single_fracture_constK_pcs_0_ts_*.vtu nodal_aperture nodal_aperture 1e-16 0
-    single_fracture_constK_pcs_0_ts_*.vtu strain_xx strain_xx 1e-16 0
-    single_fracture_constK_pcs_0_ts_*.vtu strain_yy strain_yy 1e-16 0
-    single_fracture_constK_pcs_0_ts_*.vtu strain_xy strain_xy 1e-16 0
-    single_fracture_constK_pcs_0_ts_*.vtu strain_zz strain_zz 1e-16 0
-    single_fracture_constK_pcs_0_ts_*.vtu stress_xx stress_xx 1e-16 0
-    single_fracture_constK_pcs_0_ts_*.vtu stress_yy stress_yy 1e-16 0
-    single_fracture_constK_pcs_0_ts_*.vtu stress_xy stress_xy 1e-16 0
-    single_fracture_constK_pcs_0_ts_*.vtu stress_zz stress_zz 1e-16 0
-    single_fracture_constK_pcs_0_ts_*.vtu velocity velocity 1e-16 0
+    GLOB single_fracture_constK_pcs_0_ts_*.vtu pressure pressure 1e-16 1e-16
+    GLOB single_fracture_constK_pcs_0_ts_*.vtu pressure_interpolated pressure_interpolated 1e-16 1e-16
+    GLOB single_fracture_constK_pcs_0_ts_*.vtu displacement displacement 1e-16 0
+    GLOB single_fracture_constK_pcs_0_ts_*.vtu displacement_jump1 displacement_jump1 1e-16 0
+    GLOB single_fracture_constK_pcs_0_ts_*.vtu nodal_w nodal_w 1e-16 0
+    GLOB single_fracture_constK_pcs_0_ts_*.vtu nodal_aperture nodal_aperture 1e-16 0
+    GLOB single_fracture_constK_pcs_0_ts_*.vtu strain_xx strain_xx 1e-16 0
+    GLOB single_fracture_constK_pcs_0_ts_*.vtu strain_yy strain_yy 1e-16 0
+    GLOB single_fracture_constK_pcs_0_ts_*.vtu strain_xy strain_xy 1e-16 0
+    GLOB single_fracture_constK_pcs_0_ts_*.vtu strain_zz strain_zz 1e-16 0
+    GLOB single_fracture_constK_pcs_0_ts_*.vtu stress_xx stress_xx 1e-16 0
+    GLOB single_fracture_constK_pcs_0_ts_*.vtu stress_yy stress_yy 1e-16 0
+    GLOB single_fracture_constK_pcs_0_ts_*.vtu stress_xy stress_xy 1e-16 0
+    GLOB single_fracture_constK_pcs_0_ts_*.vtu stress_zz stress_zz 1e-16 0
+    GLOB single_fracture_constK_pcs_0_ts_*.vtu velocity velocity 1e-16 0
 )
 
 AddTest(
@@ -181,21 +181,21 @@ AddTest(
     TESTER vtkdiff
     REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
     DIFF_DATA
-    TaskB_constant_pcs_0_ts_*.vtu pressure pressure 1e-16 1e-16
-    TaskB_constant_pcs_0_ts_*.vtu pressure_interpolated pressure_interpolated 1e-16 1e-16
-    TaskB_constant_pcs_0_ts_*.vtu displacement displacement 1e-16 0
-    TaskB_constant_pcs_0_ts_*.vtu displacement_jump1 displacement_jump1 1e-16 0
-    TaskB_constant_pcs_0_ts_*.vtu nodal_w nodal_w 1e-16 0
-    TaskB_constant_pcs_0_ts_*.vtu nodal_aperture nodal_aperture 1e-16 0
-    TaskB_constant_pcs_0_ts_*.vtu strain_xx strain_xx 1e-16 0
-    TaskB_constant_pcs_0_ts_*.vtu strain_yy strain_yy 1e-16 0
-    TaskB_constant_pcs_0_ts_*.vtu strain_xy strain_xy 1e-16 0
-    TaskB_constant_pcs_0_ts_*.vtu strain_zz strain_zz 1e-16 0
-    TaskB_constant_pcs_0_ts_*.vtu stress_xx stress_xx 1e-16 0
-    TaskB_constant_pcs_0_ts_*.vtu stress_yy stress_yy 1e-16 0
-    TaskB_constant_pcs_0_ts_*.vtu stress_xy stress_xy 1e-16 0
-    TaskB_constant_pcs_0_ts_*.vtu stress_zz stress_zz 1e-16 0
-    TaskB_constant_pcs_0_ts_*.vtu velocity velocity 1e-16 0
+    GLOB TaskB_constant_pcs_0_ts_*.vtu pressure pressure 1e-16 1e-16
+    GLOB TaskB_constant_pcs_0_ts_*.vtu pressure_interpolated pressure_interpolated 1e-16 1e-16
+    GLOB TaskB_constant_pcs_0_ts_*.vtu displacement displacement 1e-16 0
+    GLOB TaskB_constant_pcs_0_ts_*.vtu displacement_jump1 displacement_jump1 1e-16 0
+    GLOB TaskB_constant_pcs_0_ts_*.vtu nodal_w nodal_w 1e-16 0
+    GLOB TaskB_constant_pcs_0_ts_*.vtu nodal_aperture nodal_aperture 1e-16 0
+    GLOB TaskB_constant_pcs_0_ts_*.vtu strain_xx strain_xx 1e-16 0
+    GLOB TaskB_constant_pcs_0_ts_*.vtu strain_yy strain_yy 1e-16 0
+    GLOB TaskB_constant_pcs_0_ts_*.vtu strain_xy strain_xy 1e-16 0
+    GLOB TaskB_constant_pcs_0_ts_*.vtu strain_zz strain_zz 1e-16 0
+    GLOB TaskB_constant_pcs_0_ts_*.vtu stress_xx stress_xx 1e-16 0
+    GLOB TaskB_constant_pcs_0_ts_*.vtu stress_yy stress_yy 1e-16 0
+    GLOB TaskB_constant_pcs_0_ts_*.vtu stress_xy stress_xy 1e-16 0
+    GLOB TaskB_constant_pcs_0_ts_*.vtu stress_zz stress_zz 1e-16 0
+    GLOB TaskB_constant_pcs_0_ts_*.vtu velocity velocity 1e-16 0
 )
 
 AddTest(
@@ -207,21 +207,21 @@ AddTest(
     TESTER vtkdiff
     REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
     DIFF_DATA
-    TaskB_slip_pcs_0_ts_*.vtu pressure pressure 1e-16 1e-16
-    TaskB_slip_pcs_0_ts_*.vtu pressure_interpolated pressure_interpolated 1e-16 1e-16
-    TaskB_slip_pcs_0_ts_*.vtu displacement displacement 1e-16 0
-    TaskB_slip_pcs_0_ts_*.vtu displacement_jump1 displacement_jump1 1e-16 0
-    TaskB_slip_pcs_0_ts_*.vtu nodal_w nodal_w 1e-16 0
-    TaskB_slip_pcs_0_ts_*.vtu nodal_aperture nodal_aperture 1e-16 0
-    TaskB_slip_pcs_0_ts_*.vtu strain_xx strain_xx 1e-16 0
-    TaskB_slip_pcs_0_ts_*.vtu strain_yy strain_yy 1e-16 0
-    TaskB_slip_pcs_0_ts_*.vtu strain_xy strain_xy 1e-16 0
-    TaskB_slip_pcs_0_ts_*.vtu strain_zz strain_zz 1e-16 0
-    TaskB_slip_pcs_0_ts_*.vtu stress_xx stress_xx 1e-16 0
-    TaskB_slip_pcs_0_ts_*.vtu stress_yy stress_yy 1e-16 0
-    TaskB_slip_pcs_0_ts_*.vtu stress_xy stress_xy 1e-16 0
-    TaskB_slip_pcs_0_ts_*.vtu stress_zz stress_zz 1e-16 0
-    TaskB_slip_pcs_0_ts_*.vtu velocity velocity 1e-16 0
+    GLOB TaskB_slip_pcs_0_ts_*.vtu pressure pressure 1e-16 1e-16
+    GLOB TaskB_slip_pcs_0_ts_*.vtu pressure_interpolated pressure_interpolated 1e-16 1e-16
+    GLOB TaskB_slip_pcs_0_ts_*.vtu displacement displacement 1e-16 0
+    GLOB TaskB_slip_pcs_0_ts_*.vtu displacement_jump1 displacement_jump1 1e-16 0
+    GLOB TaskB_slip_pcs_0_ts_*.vtu nodal_w nodal_w 1e-16 0
+    GLOB TaskB_slip_pcs_0_ts_*.vtu nodal_aperture nodal_aperture 1e-16 0
+    GLOB TaskB_slip_pcs_0_ts_*.vtu strain_xx strain_xx 1e-16 0
+    GLOB TaskB_slip_pcs_0_ts_*.vtu strain_yy strain_yy 1e-16 0
+    GLOB TaskB_slip_pcs_0_ts_*.vtu strain_xy strain_xy 1e-16 0
+    GLOB TaskB_slip_pcs_0_ts_*.vtu strain_zz strain_zz 1e-16 0
+    GLOB TaskB_slip_pcs_0_ts_*.vtu stress_xx stress_xx 1e-16 0
+    GLOB TaskB_slip_pcs_0_ts_*.vtu stress_yy stress_yy 1e-16 0
+    GLOB TaskB_slip_pcs_0_ts_*.vtu stress_xy stress_xy 1e-16 0
+    GLOB TaskB_slip_pcs_0_ts_*.vtu stress_zz stress_zz 1e-16 0
+    GLOB TaskB_slip_pcs_0_ts_*.vtu velocity velocity 1e-16 0
 )
 
 AddTest(
@@ -233,19 +233,19 @@ AddTest(
     TESTER vtkdiff
     REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
     DIFF_DATA
-    TaskB_MC_slip_pcs_0_ts_*.vtu pressure pressure 1e-16 1e-16
-    TaskB_MC_slip_pcs_0_ts_*.vtu pressure_interpolated pressure_interpolated 1e-16 1e-16
-    TaskB_MC_slip_pcs_0_ts_*.vtu displacement displacement 1e-16 0
-    TaskB_MC_slip_pcs_0_ts_*.vtu displacement_jump1 displacement_jump1 1e-16 0
-    TaskB_MC_slip_pcs_0_ts_*.vtu nodal_w nodal_w 1e-16 0
-    TaskB_MC_slip_pcs_0_ts_*.vtu nodal_aperture nodal_aperture 1e-16 0
-    TaskB_MC_slip_pcs_0_ts_*.vtu strain_xx strain_xx 1e-16 0
-    TaskB_MC_slip_pcs_0_ts_*.vtu strain_yy strain_yy 1e-16 0
-    TaskB_MC_slip_pcs_0_ts_*.vtu strain_xy strain_xy 1e-16 0
-    TaskB_MC_slip_pcs_0_ts_*.vtu strain_zz strain_zz 1e-16 0
-    TaskB_MC_slip_pcs_0_ts_*.vtu stress_xx stress_xx 1e-16 0
-    TaskB_MC_slip_pcs_0_ts_*.vtu stress_yy stress_yy 1e-16 0
-    TaskB_MC_slip_pcs_0_ts_*.vtu stress_xy stress_xy 1e-16 0
-    TaskB_MC_slip_pcs_0_ts_*.vtu stress_zz stress_zz 1e-16 0
-    TaskB_MC_slip_pcs_0_ts_*.vtu velocity velocity 1e-16 0
+    GLOB TaskB_MC_slip_pcs_0_ts_*.vtu pressure pressure 1e-16 1e-16
+    GLOB TaskB_MC_slip_pcs_0_ts_*.vtu pressure_interpolated pressure_interpolated 1e-16 1e-16
+    GLOB TaskB_MC_slip_pcs_0_ts_*.vtu displacement displacement 1e-16 0
+    GLOB TaskB_MC_slip_pcs_0_ts_*.vtu displacement_jump1 displacement_jump1 1e-16 0
+    GLOB TaskB_MC_slip_pcs_0_ts_*.vtu nodal_w nodal_w 1e-16 0
+    GLOB TaskB_MC_slip_pcs_0_ts_*.vtu nodal_aperture nodal_aperture 1e-16 0
+    GLOB TaskB_MC_slip_pcs_0_ts_*.vtu strain_xx strain_xx 1e-16 0
+    GLOB TaskB_MC_slip_pcs_0_ts_*.vtu strain_yy strain_yy 1e-16 0
+    GLOB TaskB_MC_slip_pcs_0_ts_*.vtu strain_xy strain_xy 1e-16 0
+    GLOB TaskB_MC_slip_pcs_0_ts_*.vtu strain_zz strain_zz 1e-16 0
+    GLOB TaskB_MC_slip_pcs_0_ts_*.vtu stress_xx stress_xx 1e-16 0
+    GLOB TaskB_MC_slip_pcs_0_ts_*.vtu stress_yy stress_yy 1e-16 0
+    GLOB TaskB_MC_slip_pcs_0_ts_*.vtu stress_xy stress_xy 1e-16 0
+    GLOB TaskB_MC_slip_pcs_0_ts_*.vtu stress_zz stress_zz 1e-16 0
+    GLOB TaskB_MC_slip_pcs_0_ts_*.vtu velocity velocity 1e-16 0
 )
