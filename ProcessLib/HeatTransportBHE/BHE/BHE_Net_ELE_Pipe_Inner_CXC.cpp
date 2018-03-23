6ee209a694ad1543ec3506ceab80134d3ef441d2
ChaofanChen <chaofan.chen@ufz.de>
2018-03-23 16:55:27 +0100

    Add BHE type of CXA and CXC

---
+++ ProcessLib/HeatTransportBHE/BHE/BHE_Net_ELE_Pipe_Inner_CXC.cpp
@@ -0,0 +1,30 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2017, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#include "BHE_Net_ELE_Pipe_Inner_CXC.h"
+
+using namespace BHE;
+
+BHE_Net_ELE_Pipe_Inner_CXC::BHE_Net_ELE_Pipe_Inner_CXC(std::string& name,
+                                                       BHE::BHEAbstract* m_BHE)
+    : BHE_Net_ELE_Pipe(name, BHE_NET_ELE::BHE_NET_PIPE_INNER_CXC), _m_BHE(m_BHE)
+{
+    // configure the penalty factor
+    this->set_penalty_factor(1.0e6);
+}
+
+std::size_t BHE::BHE_Net_ELE_Pipe_Inner_CXC::get_global_idx_in()
+{
+    return _global_idx_in;
+}
+
+std::size_t BHE::BHE_Net_ELE_Pipe_Inner_CXC::get_global_idx_out()
+{
+    return _global_idx_out;
+}
---
+++ ProcessLib/HeatTransportBHE/BHE/BHE_Net_ELE_Pipe_Inner_CXC.h
@@ -0,0 +1,54 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2017, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#pragma once
+
+#include "BHEAbstract.h"
+#include "BHE_Net_ELE_Abstract.h"
+#include "BHE_Net_ELE_Pipe.h"
+
+namespace BHE  // namespace of borehole heat exchanger
+{
+class BHE_Net_ELE_Pipe_Inner_CXC : public BHE_Net_ELE_Pipe
+{
+public:
+    /**
+     * constructor
+     */
+    BHE_Net_ELE_Pipe_Inner_CXC(std::string& name, BHE::BHEAbstract* m_BHE);
+
+protected:
+    /**
+     * obtain the global index at the pipeline inlet
+     */
+    std::size_t get_global_idx_in();
+
+    /**
+     * obtain the global index at the pipeline outlet
+     */
+    std::size_t get_global_idx_out();
+
+private:
+    /**
+     * the global index at the pipeline inlet
+     */
+    std::size_t _global_idx_in;
+
+    /**
+     * the global index at the pipeline outlet
+     */
+    std::size_t _global_idx_out;
+
+    /**
+     * the BHE which this pipeline is applied on
+     */
+    const BHE::BHEAbstract* _m_BHE;
+};
+
+}  // namespace BHE
