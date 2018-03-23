111d9c6267f549bc8b6545524f4d7a48b34cc560
ChaofanChen <chaofan.chen@ufz.de>
2018-03-23 16:58:37 +0100

    Add BHE type of CXA and CXC

--- ProcessLib/HeatTransportBHE/BHE/BHE_Net_ELE_Pipe_Inner_CXA.cpp
+++ ProcessLib/HeatTransportBHE/BHE/BHE_Net_ELE_Pipe_Inner_CXA.cpp
@@ -1,22 +1,24 @@
 /**
- * \copyright
- * Copyright (c) 2012-2017, OpenGeoSys Community (http://www.opengeosys.org)
- *            Distributed under a Modified BSD License.
- *              See accompanying file LICENSE.txt or
- *              http://www.opengeosys.org/project/license
- *
- */
+* \copyright
+* Copyright (c) 2012-2017, OpenGeoSys Community (http://www.opengeosys.org)
+*            Distributed under a Modified BSD License.
+*              See accompanying file LICENSE.txt or
+*              http://www.opengeosys.org/project/license
+*
+*/
 
 #include "BHE_Net_ELE_Pipe_Inner_CXA.h"
 
 using namespace BHE;
 
-BHE_Net_ELE_Pipe_Inner_CXA::BHE_Net_ELE_Pipe_Inner_CXA(std::string& name,
-                                                       BHE::BHEAbstract* m_BHE)
-    : BHE_Net_ELE_Pipe(name, BHE_NET_ELE::BHE_NET_PIPE_INNER_CXA), _m_BHE(m_BHE)
+BHE_Net_ELE_Pipe_Inner_CXA::BHE_Net_ELE_Pipe_Inner_CXA(std::string & name, BHE::BHEAbstract * m_BHE)
+    : BHE_Net_ELE_Pipe(name, BHE_NET_ELE::BHE_NET_PIPE_INNER_CXA),
+    _m_BHE(m_BHE)
 {
+
     // configure the penalty factor
     this->set_penalty_factor(1.0e6);
+
 }
 
 std::size_t BHE::BHE_Net_ELE_Pipe_Inner_CXA::get_global_idx_in()
---
+++ ProcessLib/HeatTransportBHE/BHE/CreateBHECXA.h
@@ -0,0 +1,24 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2017, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#pragma once
+
+#include "BHE_CXA.h"
+#include "ProcessLib/Process.h"
+
+namespace BHE  // namespace of borehole heat exchanger
+{
+BHE::BHE_CXA* CreateBHECXA(
+    BaseLib::ConfigTree const& config,
+    BaseLib::ConfigTree const& bhe_conf,
+    std::map<std::string,
+             std::unique_ptr<MathLib::PiecewiseLinearInterpolation>> const&
+        curves);
+
+}  // namespace BHE
