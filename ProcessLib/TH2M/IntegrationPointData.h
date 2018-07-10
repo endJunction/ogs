84e3b58c7688d38ede87477cf254415fa90b4697
Dmitry Yu. Naumov <github@naumov.de>
2018-07-10 16:06:07 +0200

    Extract IntegrationPointData in own header.

---
+++ ProcessLib/TH2M/IntegrationPointData.h
@@ -0,0 +1,114 @@
+/**
+ * \file
+ *
+ * \copyright
+ * Copyright (c) 2012-2018, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#pragma once
+
+#include <algorithm>
+#include <memory>
+#include <vector>
+
+#include "MaterialLib/SolidModels/LinearElasticIsotropic.h"
+#include "MathLib/KelvinVector.h"
+#include "MathLib/LinAlg/Eigen/EigenMapTools.h"
+#include "NumLib/DOF/DOFTableUtil.h"
+#include "NumLib/Fem/ShapeMatrixPolicy.h"
+#include "NumLib/Function/Interpolation.h"
+#include "ProcessLib/Deformation/BMatrixPolicy.h"
+#include "ProcessLib/Deformation/LinearBMatrix.h"
+#include "ProcessLib/LocalAssemblerTraits.h"
+#include "ProcessLib/Parameter/Parameter.h"
+#include "ProcessLib/Utils/InitShapeMatrices.h"
+
+#include "TH2MProcessData.h"
+
+namespace ProcessLib
+{
+namespace TH2M
+{
+namespace MPL = MaterialPropertyLib;
+
+template <typename BMatricesType, typename ShapeMatrixTypeDisplacement,
+          typename ShapeMatricesTypePressure, int DisplacementDim, int NPoints>
+struct IntegrationPointData final
+{
+    explicit IntegrationPointData(
+        MaterialLib::Solids::MechanicsBase<DisplacementDim>& solid_material)
+        : solid_material(solid_material),
+          material_state_variables(
+              solid_material.createMaterialStateVariables())
+    {
+    }
+
+    typename ShapeMatrixTypeDisplacement::template MatrixType<
+        DisplacementDim, NPoints * DisplacementDim>
+        N_u_op;
+    typename BMatricesType::KelvinVectorType sigma_eff, sigma_eff_prev;
+    typename BMatricesType::KelvinVectorType eps, eps_prev;
+
+    // typename BMatricesType::KelvinVectorType sigma_prev;
+
+    typename ShapeMatrixTypeDisplacement::NodalRowVectorType N_u;
+    typename ShapeMatrixTypeDisplacement::GlobalDimNodalMatrixType dNdx_u;
+
+    typename ShapeMatricesTypePressure::NodalRowVectorType N_p;
+    typename ShapeMatricesTypePressure::GlobalDimNodalMatrixType dNdx_p;
+
+    using GlobalDimVectorType =
+        typename ShapeMatricesTypePressure::GlobalDimVectorType;
+
+    MaterialLib::Solids::MechanicsBase<DisplacementDim>& solid_material;
+    std::unique_ptr<typename MaterialLib::Solids::MechanicsBase<
+        DisplacementDim>::MaterialStateVariables>
+        material_state_variables;
+    double integration_weight;
+
+    // output vector for wetting phase saturation and pressure with
+    // respect to each integration point
+    double saturation;
+    double pressure_wet;
+    double density_gas;
+    double density_liquid;
+    double pressure_gas_linear;
+    double pressure_cap_linear;
+    GlobalDimVectorType velocity_gas;
+    GlobalDimVectorType velocity_liquid;
+    void pushBackState()
+    {
+        eps_prev = eps;
+        sigma_eff_prev = sigma_eff;
+        material_state_variables->pushBackState();
+    }
+
+    template <typename DisplacementVectorType>
+    typename BMatricesType::KelvinMatrixType updateConstitutiveRelation(
+        double const t,
+        SpatialPosition const& x_position,
+        double const dt,
+        DisplacementVectorType const& /*u*/)
+    {
+        auto&& solution = solid_material.integrateStress(
+            t, x_position, dt, eps_prev, eps, sigma_eff_prev,
+            *material_state_variables);
+
+        if (!solution)
+            OGS_FATAL("Computation of local constitutive relation failed.");
+
+        MathLib::KelvinVector::KelvinMatrixType<DisplacementDim> C;
+        std::tie(sigma_eff, material_state_variables, C) = std::move(*solution);
+
+        return C;
+    }
+
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+};
+
+}  // namespace TH2M
+}  // namespace ProcessLib
--- ProcessLib/TH2M/TH2M_FEM.h
+++ ProcessLib/TH2M/TH2M_FEM.h
@@ -25,9 +25,9 @@
 #include "ProcessLib/Parameter/Parameter.h"
 #include "ProcessLib/Utils/InitShapeMatrices.h"
 
+#include "IntegrationPointData.h"
 #include "LocalAssemblerInterface.h"
 #include "TH2MProcessData.h"
-
 
 #include <iostream>
 
@@ -36,68 +36,6 @@ namespace ProcessLib
 namespace TH2M
 {
 namespace MPL = MaterialPropertyLib;
-
-template <typename BMatricesType, typename ShapeMatrixTypeDisplacement,
-          typename ShapeMatricesTypePressure, int DisplacementDim, int NPoints>
-struct IntegrationPointData final
-{
-    explicit IntegrationPointData(
-        MaterialLib::Solids::MechanicsBase<DisplacementDim>& solid_material)
-        : solid_material(solid_material),
-          material_state_variables(
-              solid_material.createMaterialStateVariables())
-    {
-    }
-
-    typename ShapeMatrixTypeDisplacement::template MatrixType<
-        DisplacementDim, NPoints * DisplacementDim>
-        N_u_op;
-    typename BMatricesType::KelvinVectorType sigma_eff, sigma_eff_prev;
-    typename BMatricesType::KelvinVectorType eps, eps_prev;
-
-    // typename BMatricesType::KelvinVectorType sigma_prev;
-
-    typename ShapeMatrixTypeDisplacement::NodalRowVectorType N_u;
-    typename ShapeMatrixTypeDisplacement::GlobalDimNodalMatrixType dNdx_u;
-
-    typename ShapeMatricesTypePressure::NodalRowVectorType N_p;
-    typename ShapeMatricesTypePressure::GlobalDimNodalMatrixType dNdx_p;
-
-    MaterialLib::Solids::MechanicsBase<DisplacementDim>& solid_material;
-    std::unique_ptr<typename MaterialLib::Solids::MechanicsBase<
-        DisplacementDim>::MaterialStateVariables>
-        material_state_variables;
-    double integration_weight;
-
-    void pushBackState()
-    {
-        eps_prev = eps;
-        sigma_eff_prev = sigma_eff;
-        material_state_variables->pushBackState();
-    }
-
-    template <typename DisplacementVectorType>
-    typename BMatricesType::KelvinMatrixType updateConstitutiveRelation(
-        double const t,
-        SpatialPosition const& x_position,
-        double const dt,
-        DisplacementVectorType const& /*u*/)
-    {
-        auto&& solution = solid_material.integrateStress(
-            t, x_position, dt, eps_prev, eps, sigma_eff_prev,
-            *material_state_variables);
-
-        if (!solution)
-            OGS_FATAL("Computation of local constitutive relation failed.");
-
-        MathLib::KelvinVector::KelvinMatrixType<DisplacementDim> C;
-        std::tie(sigma_eff, material_state_variables, C) = std::move(*solution);
-
-        return C;
-    }
-
-    EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
-};
 
 /// Used by for extrapolation of the integration point values. It is ordered
 /// (and stored) by integration points.
