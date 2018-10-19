a460a89ee093c47621423ae0f2c5a78ce785d699
Dmitry Yu. Naumov <github@naumov.de>
2018-10-19 16:49:26 +0200

    [T] CXA; Reduce end-time for ctest. Add ctest.

--- ProcessLib/HeatTransportBHE/Tests.cmake
+++ ProcessLib/HeatTransportBHE/Tests.cmake
@@ -1,5 +1,5 @@
 AddTest(
-    NAME HeatTransportBHE_3D_beier_sandbox
+    NAME HeatTransportBHE_1U_3D_beier_sandbox
     PATH Parabolic/T/3D_Beier_sandbox
     EXECUTABLE ogs
     EXECUTABLE_ARGS beier_sandbox.prj
@@ -7,7 +7,18 @@ AddTest(
     TESTER vtkdiff
     REQUIREMENTS NOT OGS_USE_MPI
     DIFF_DATA
-    beier_sandbox_pcs_0_ts_10_t_600.000000.vtu
-    beier_sandbox_pcs_0_ts_10_t_600.000000.vtu temperature_BHE1 temperature_BHE1 0 5e-15
+    beier_sandbox_pcs_0_ts_10_t_600.000000.vtu beier_sandbox_pcs_0_ts_10_t_600.000000.vtu temperature_BHE1 temperature_BHE1 0 5e-15
     beier_sandbox_pcs_0_ts_10_t_600.000000.vtu beier_sandbox_pcs_0_ts_10_t_600.000000.vtu temperature_soil temperature_soil 0 1e-13
+)
+AddTest(
+    NAME HeatTransportBHE_CXA_3D_deep_borehole
+    PATH Parabolic/T/3D_deep_borehole_heat_exchanger
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS deep_borehole_heat_exchanger.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    DIFF_DATA
+    deep_borehole_heat_exchanger_pcs_0_ts_10_t_600.000000.vtu deep_borehole_heat_exchanger_pcs_0_ts_10_t_600.000000.vtu temperature_BHE1 temperature_BHE1 0 1e-14
+    deep_borehole_heat_exchanger_pcs_0_ts_10_t_600.000000.vtu deep_borehole_heat_exchanger_pcs_0_ts_10_t_600.000000.vtu temperature_soil temperature_soil 0 1e-13
 )
