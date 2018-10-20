4452c28370e9976053a252eead27a0e7c7cd8ff0
Dmitri Naumov <github@naumov.de>
2018-10-21 00:07:17 +0200

    [T] BHE 1U; Constant flow/fixed power.

--- ProcessLib/HeatTransportBHE/Tests.cmake
+++ ProcessLib/HeatTransportBHE/Tests.cmake
@@ -10,6 +10,19 @@ AddTest(
     beier_sandbox_pcs_0_ts_10_t_600.000000.vtu beier_sandbox_pcs_0_ts_10_t_600.000000.vtu temperature_BHE1 temperature_BHE1 0 5e-15
     beier_sandbox_pcs_0_ts_10_t_600.000000.vtu beier_sandbox_pcs_0_ts_10_t_600.000000.vtu temperature_soil temperature_soil 0 1e-13
 )
+
+AddTest(
+    NAME HeatTransportBHE_1U_beier_sandbox_fixed_power_constant_flow
+    PATH Parabolic/T/3D_Beier_sandbox
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS fixed_power_constant_flow.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    DIFF_DATA
+    fixed_power_constant_flow_pcs_0_ts_10_t_600.000000.vtu fixed_power_constant_flow_pcs_0_ts_10_t_600.000000.vtu temperature_BHE1 temperature_BHE1 0 5e-15
+    fixed_power_constant_flow_pcs_0_ts_10_t_600.000000.vtu fixed_power_constant_flow_pcs_0_ts_10_t_600.000000.vtu temperature_soil temperature_soil 0 1e-13
+)
 AddTest(
     NAME HeatTransportBHE_CXA_3D_deep_borehole
     PATH Parabolic/T/3D_deep_borehole_heat_exchanger
