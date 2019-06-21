c3b32e43e889f7c324067cc7b2e95e768851c7e1
Dmitri Naumov <dmitri.naumov@ufz.de>
2019-06-21 20:37:08 +0200

    WIP

--- ProcessLib/SourceTerms/VolumetricSourceTermFEM.h
+++ ProcessLib/SourceTerms/VolumetricSourceTermFEM.h
@@ -75,9 +75,9 @@ public:
             _integration_method.getNumberOfPoints();
 
         auto const shape_matrices =
-                       initShapeMatrices<ShapeFunction, ShapeMatricesType,
-                                         IntegrationMethod, GlobalDim>(
-                           element, is_axially_symmetric, _integration_method);
+            initShapeMatrices<ShapeFunction, ShapeMatricesType,
+                              IntegrationMethod, 3>(
+                element, is_axially_symmetric, _integration_method);
 
         for (unsigned ip = 0; ip < n_integration_points; ip++)
         {
