92d82ed1a3467fb4a1e8e2dd5fdb627802c0bf04
Dmitri Naumov <dmitri.naumov@ufz.de>
2019-08-20 18:48:31 +0200

    Rename variable kelvin_vector_size.
    Wrong notation was used before.

--- MaterialLib/SolidModels/LinearElasticOrthotropic.h
+++ MaterialLib/SolidModels/LinearElasticOrthotropic.h
@@ -123,7 +123,7 @@ public:
     };
 
 public:
-    static int const KelvinVectorSize =
+    static int const kelvin_vector_size =
         MathLib::KelvinVector::size<DisplacementDim>();
     using KelvinVector =
         MathLib::KelvinVector::KelvinVectorType<DisplacementDim>;
